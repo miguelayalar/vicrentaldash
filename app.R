e18d0a3a3a4a97b103788dd604f664237caa8eb1
miguel ayala <miguel.ayala.au@gmail.com>
2025-01-27 15:39:36 +1000

    make dash into package

--- app.R
+++ app.R
@@ -9,21 +9,6 @@ library(sf)
 library(leafpop)
 
 
-# load resources ----
-source("R/ui.R")
-source("R/badges.R")
-source("R/load_data.R")
-source("R/theme.R")
-source("R/page_rents.R")
-source("R/page_vacancy.R")
-source("R/page_gccsa_rents.R")
-source("R/page_maps.R")
-source("R/viz_vic_rents.R")
-source("R/viz_vic_vacancies.R")
-source("R/viz_gccsa_rents.R")
-source("R/viz_map_lga.R")
-
-
 
 # Run the application ----- 
 shinyApp(ui = ui, server = server)
