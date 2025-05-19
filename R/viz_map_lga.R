bb84f505cb91c4985cde39bac93bf118334dc13c
miguel ayala <miguel.ayala.au@gmail.com>
2025-05-19 22:54:54 +1100

    improve info control

--- R/viz_map_lga.R
+++ R/viz_map_lga.R
@@ -14,17 +14,32 @@ leaflet_vic_lga <- function(data = datalist$map_rents,
   
   pal <- colorBin("viridis", domain = df$value)
   
+  labs <- sprintf(
+    "<strong>LGA:</strong> %s<br/><strong>Median weekly rent:</strong> $%g",
+    df$lga, df$value) %>% 
+    lapply(htmltools::HTML)
+  
   leaflet() %>%
     addTiles() %>%
-    setView(lng = 144.5, lat = -37.5, zoom = 9) %>%
+    setView(lng = 144.9, lat = -37.8, zoom = 9) %>%
     addPolygons(
       data = df,
       color = ~pal(value),     # Border color
       weight = 1,          # Border weight
       opacity = .5,         # Border opacity
       fillOpacity = 0.4,   # Fill opacity
-      label = paste0("<strong>LGA: <strong>", df$lga, "<br><strong>Median weekly rent:<strong> $", df$value) %>%
-        lapply(htmltools::HTML)
+      highlightOptions = highlightOptions(
+        weight = 3,
+        color = "black",
+        #dashArray = "",
+        #fillOpacity = 0.7,
+        bringToFront = TRUE
+        ),
+      label = labs,
+      labelOptions = labelOptions(
+        style = list("font-weight" = "normal", padding = "3px 8px"),
+        textsize = "15px",
+        direction = "auto")
     ) %>% 
     leaflet::addLegend(data = df,
               position = "bottomright",
