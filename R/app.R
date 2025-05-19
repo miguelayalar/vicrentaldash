3710b0d86532710e50df97a0a3d2740813cdab71
miguel ayala <miguel.ayala.au@gmail.com>
2025-05-19 23:28:50 +1100

    fix deployment

--- R/app.R
+++ R/app.R
@@ -1,24 +1,31 @@
 
+monthApp <- function(...) {
 
-# Load objects from this project
-pkgload::load_all(".")
-
-# Define global variables available to UI and server
-datalist$rental %>% 
-  dplyr::filter(!lga %in% c("Group Total", "Greater Melbourne", "Regional Victoria", "Victoria")) %>% 
-  dplyr::distinct(lga)  %>%
-  dplyr::pull(lga) %>%
-  assign("lgas", ., envir = .GlobalEnv)
-
-datalist$rental %>% 
-  dplyr::distinct(region)  %>%
-  dplyr::pull(region) %>%
-  assign("regions", ., envir = .GlobalEnv)
-
-datalist$vacancy %>% dplyr::select(-1) %>% names() %>% 
-  assign("areas", ., envir = .GlobalEnv)
-
-
-
-# Run the application ----- 
-shinyApp(ui = ui, server = server)
+  # Load objects from this project
+  pkgload::load_all(".")
+  
+  # Define global variables available to UI and server
+  datalist$rental %>%
+    dplyr::filter(!lga %in% c(
+      "Group Total",
+      "Greater Melbourne",
+      "Regional Victoria",
+      "Victoria"
+    )) %>%
+    dplyr::distinct(lga)  %>%
+    dplyr::pull(lga) %>%
+    assign("lgas", ., envir = .GlobalEnv)
+  
+  datalist$rental %>%
+    dplyr::distinct(region)  %>%
+    dplyr::pull(region) %>%
+    assign("regions", ., envir = .GlobalEnv)
+  
+  datalist$vacancy %>% dplyr::select(-1) %>% names() %>%
+    assign("areas", ., envir = .GlobalEnv)
+  
+  
+  
+  # Run the application -----
+  shinyApp(ui = ui, server = server)
+}
